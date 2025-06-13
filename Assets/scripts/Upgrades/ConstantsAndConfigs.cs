b20fe2c922e029d59d560c186562f4d6f04b6545
Andrey Filin <filin.aa@phystech.edu>
2025-06-14 00:18:17 +0300

    анимация бега

--- Assets/scripts/Upgrades/ConstantsAndConfigs.cs
+++ Assets/scripts/Upgrades/ConstantsAndConfigs.cs
@@ -3,15 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConstantsAndConfigs
-{
-    public const String MIN_RADIUS_STAT_NAME = "MinRadius";
-    public const String MAX_RADIUS_STAT_NAME = "MaxRadius";
-    public const String SPEED_STAT_NAME = "Speed";
+    public class ConstantsAndConfigs
+    {
+        public const String MIN_RADIUS_STAT_NAME = "MinRadius";
+        public const String MAX_RADIUS_STAT_NAME = "MaxRadius";
+        public const String SPEED_STAT_NAME = "Speed";
 
-    public const float MIN_RADIUS_DEFAULT = 4.0f;
-    public const float MAX_RADIUS_DEFAULT = 5.0f;
-    public const float SPEED_MULTIPLIER_DEFAULT = 1.0f;
+        public const float MIN_RADIUS_DEFAULT = 4.0f;
+        public const float MAX_RADIUS_DEFAULT = 5.0f;
+        public const float SPEED_MULTIPLIER_DEFAULT = 1.0f;
 
-    public const float UPGRADE_EXPONENTIAL_MULTIPLIER = 1.1f;
-}
+        public const float UPGRADE_EXPONENTIAL_MULTIPLIER = 1.1f;
+    }
+
+
