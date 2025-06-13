5e7f052ad07c91dc8e48de1797cbc41a99a0b27f
Andrey Filin <filin.aa@phystech.edu>
2025-06-13 20:51:44 +0300

    пофикшена анимация при рубке

--- Assets/scripts/PrecisionGame.cs
+++ Assets/scripts/PrecisionGame.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PrecisionGame : MonoBehaviour
 {
+    [SerializeField] private Transform player;
     
-    [SerializeField] private AudioClip hitSound; // Звуковой клип для нажатия E
-    [SerializeField] private AudioSource audioSource; // Источник звука
+
+    [SerializeField] private float  razmaxTime=0.3f;
+    [SerializeField] private float  hitTime=0.25f;
+    [SerializeField] private GameObject instance;
     
     public RectTransform indicator; // Двигающийся индикатор
     public RectTransform hitZone; // Зона попадания
@@ -27,6 +30,7 @@ public class PrecisionGame : MonoBehaviour
         minY = -halfHeight + indicator.rect.height / 2f;
         maxY = halfHeight - indicator.rect.height / 2f;
         indicatorPos = indicator.transform.position;
+        instance.SetActive(false);
     }
 
     void Update()
@@ -38,30 +42,59 @@ public class PrecisionGame : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.E))
             {
                 inputAllowed = false;
-                PlayHitSound(); // Проигрываем звук
-                CheckResult();
+                StartCoroutine(HitCoroutine());
+                
             }
         }
     }
     
     void PlayHitSound()
     {
-        if (audioSource != null && hitSound != null)
-        {
-            audioSource.PlayOneShot(hitSound);
-        }
-        else
-        {
-            Debug.LogError("no sound to play");
-        }
+        
+            SoundController.Instance.PlayHit();
+
     }
     
-
     public void StartGame()
     {
-        gameObject.SetActive(true);
+        Debug.Log("Called start game in PrecGame");
+        StartCoroutine(RazmaxCoroutine());
+        
+    }
+    private IEnumerator RazmaxCoroutine()
+    {
+        
+        Quaternion startRotation = player.rotation;
+        Quaternion targetRotation = startRotation * Quaternion.Euler(0f, 180f, 0f);
+    
+        float elapsedTime = 0f;
+        GameManager.Instance.SetAnimatorTrigger("razmax");
+        while (elapsedTime < razmaxTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / razmaxTime; // Прогресс от 0 до 1
+            player.rotation = Quaternion.Lerp(startRotation, targetRotation, t);
+            yield return null;
+        }
+    
+        player.rotation = targetRotation;
+        Debug.Log("Coroutine finished");
+        instance.SetActive(true);
         inputAllowed = true;
     }
+
+    private IEnumerator HitCoroutine()
+    {
+        GameManager.Instance.SetAnimatorTrigger("hit");
+        
+        yield return new WaitForSeconds(hitTime);
+        PlayHitSound(); // Проигрываем звук
+        CheckResult();
+        EndGame();
+    }
+    
+    
+    
 
     void MoveIndicator()
     {
@@ -92,12 +125,12 @@ public class PrecisionGame : MonoBehaviour
             Debug.Log("❌ ПРОМАХ!");
         }
 
-        EndGame();
     }
 
     void EndGame()
     {
-        gameObject.SetActive(false);
+        Debug.Log("Game ended");
+        instance.SetActive(false);
         indicator.transform.position = indicatorPos;
         GameManager.Instance.EndPrecisionGame();
     }
--- Assets/scripts/controller/ForestController.cs
+++ Assets/scripts/controller/ForestController.cs
@@ -5,9 +5,14 @@ namespace Controller
     public class ForestController : MonoBehaviour
     {
         [SerializeField] private float moveSpeed = 5f; // Скорость движения
-
+        private Animator _animator;
         public bool IsRunning; // Движется ли персонаж
-
+        private int prevVertical = 1;
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+            
+        }
         void Update()
         {
             Move();
@@ -21,7 +26,18 @@ namespace Controller
 
             // Обновляем состояние бега
             IsRunning = !(moveX == 0 && moveZ == 0);
-
+            
+            _animator.SetBool("running", IsRunning);
+            if (moveZ != 0)
+            {
+                int vertical = moveZ <= 0 ? 1 : -1;
+                if (vertical != prevVertical)
+                {
+                    prevVertical = vertical;
+                    _animator.SetInteger("vertical", vertical);
+                }
+            }
+            
             // Формируем вектор движения
             Vector3 moveDirection = new Vector3(moveX, 0f, moveZ).normalized;
      
