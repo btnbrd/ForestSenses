ee73a23d5517effb493bc4c239ec7cba35ff1385
Andrey Filin <filin.aa@phystech.edu>
2025-06-14 01:00:04 +0300

    вынесена анимация

---
+++ Assets/scripts/ForestManagers/AnimationController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace ForestManagers
+{
+    public class AnimationController : MonoBehaviour
+    {
+        private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+        private static readonly int Vertical = Animator.StringToHash("Vertical");
+        private static readonly int Speed = Animator.StringToHash("Speed");
+        [SerializeField] private Animator animator;
+        
+        public static AnimationController Instance{get; private set;}
+
+        public void SetHorizontal()
+        {
+            animator.SetFloat(Horizontal, 0f);
+        }
+
+        public void SetVertical(int vertical)
+        {
+            animator.SetFloat(Vertical, 0f);
+        }
+
+        public void SetRunning(bool isRunning)
+        {
+            animator.SetBool(Speed, isRunning);
+        }
+
+        public void SetTrigger(string trigger)
+        {
+            
+            SetTrigger(trigger);
+            SetVertical(1);
+        }
+        
+    }
+}
--- Assets/scripts/controller/ForestController.cs
+++ Assets/scripts/controller/ForestController.cs
@@ -1,16 +1,17 @@
-﻿using UnityEngine;
+﻿using ForestManagers;
+using UnityEngine;
 
 namespace Controller
 {
     public class ForestController : MonoBehaviour
     {
         [SerializeField] private float moveSpeed = 5f; // Скорость движения
-        private Animator _animator;
+
         public bool IsRunning; // Движется ли персонаж
         private int prevVertical = 1;
         void Start()
         {
-            _animator = GetComponent<Animator>();
+
             
         }
         void Update()
@@ -27,14 +28,14 @@ namespace Controller
             // Обновляем состояние бега
             IsRunning = !(moveX == 0 && moveZ == 0);
             
-            _animator.SetBool("running", IsRunning);
+            AnimationController.Instance.SetRunning(IsRunning);
             if (moveZ != 0)
             {
                 int vertical = moveZ <= 0 ? 1 : -1;
                 if (vertical != prevVertical)
                 {
                     prevVertical = vertical;
-                    _animator.SetInteger("vertical", vertical);
+                    AnimationController.Instance.SetVertical(vertical);
                 }
             }
             
